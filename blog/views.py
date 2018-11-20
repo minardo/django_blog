9a6a1728eff2f9e1e7b3af0aca7f66482f4bdcbf
minardo <ardo15.project@gmail.com>
2018-11-20 19:48:10 +0700

    adding url patterns views

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
+
 def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
